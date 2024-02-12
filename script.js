77d2d53b198bc35368a8e61b3b0d2ca3b3a59eb0
campus000 <159609082+campus000@users.noreply.github.com>
2024-02-12 14:59:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function printReceipt() {
     const receiptContent = generateReceiptContent(); 
 alert(receiptContent);
   alert("a");
-  sendTextData(receiptContent) // Send the generated content for printing        .then(() => {
+  sendTextData("abcdef") // Send the generated content for printing        .then(() => {
             progress.hidden = true; // Hide progress indicator or handle success
         })
         .catch(handleError); // Handle errors if any
