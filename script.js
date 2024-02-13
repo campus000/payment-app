a17d181f64ba39082895ed6708d69699da25722e
campus000 <159609082+campus000@users.noreply.github.com>
2024-02-13 15:39:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -413,7 +413,9 @@ const itemName = item.toString(); // Use the item itself as the name
 const itemCost = addedItems[item].price * addedItems[item].quantity;
 
 // Item Row with adjusted spacing and line break for long item names
-const itemRow = `${itemName}${addedItems[item].quantity.toString()}${itemCost.toFixed(2)}\n`;
+//const itemRow = `${itemName}${addedItems[item].quantity.toString()}${itemCost.toFixed(2)}\n`;
+  const itemRow = `${itemName.slice(0, 10).padEnd(12)}${addedItems[item].quantity.toString().padEnd(12)}${itemCost.toFixed(2)}\n`;
+content += itemRow;
 content += itemRow;
 
 // If the item name is too long, add the remaining part on the next line
