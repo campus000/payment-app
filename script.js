b7e7b6904b2365f55a2ba2f0039bb8251cec2203
campus000 <159609082+campus000@users.noreply.github.com>
2024-02-14 11:07:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+ 'use strict';
 document.addEventListener('WebComponentsReady', function () {
   let progress = document.querySelector('#progress');
   let dialog = document.querySelector('#dialog');
